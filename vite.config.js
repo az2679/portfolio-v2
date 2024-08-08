d0c9071255fe009bda48953e0c6e74293bab7fa7
Athena <az2679@nyu.edu>
2024-08-08 19:31:41 -0400

    build

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   server: {
     port: 3001,
   },
   plugins: [react()],
+  publicDir: 'public',
+  base: '/portfolio-v2/',
+  build: {
+    outDir: 'docs',
+  },
 });
